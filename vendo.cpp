a676728c6718dfa020a2a05f06ac7979ec591db8
jrvmbust <jrvmbust@hotmail.com>
2015-07-09 21:01:55 +0800

    something

--- vendo.cpp
+++ vendo.cpp
@@ -5,6 +5,7 @@
 /*Vendo Machine Assignment #2 for ITE-001*/
 /*Coded by John Rovic Vargas IT11KA3*/
 //shoutout dyan kina mike at aaron na tumingin dito sa code ko
+// ung mga while loop kahit hindi na ilagay un. basta proper selections lang ung ilagay.
 
 const int jpprice = 13; 
 const int scprice = 35;
