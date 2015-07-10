daf60cbeba96d7c4c96fb5e6d1cafa8da1eb70ae
jrvmbust <jrvmbust@hotmail.com>
2015-07-10 12:00:18 +0800

    BAWAL DAW WHILE LOOPS
    S A D B O I S

--- vendo.cpp
+++ vendo.cpp
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <conio.h>
-#include <stdlib.h> // atoi
+
+//#include <stdlib.h> //atoi (FOR ALPHANUMERIC INPUT CHECKING)
 
 /*Vendo Machine Assignment #2 for ITE-001*/
 /*Coded by John Rovic Vargas IT11KA3*/
 //shoutout dyan kina mike at aaron na tumingin dito sa code ko
 // ung mga while loop kahit hindi na ilagay un. basta proper selections lang ung ilagay.
+//i wrote this code before someone asked kung pwede daw.
+//UPDATE - bawal daw while loops. s a d b o i s
 
 const int jpprice = 13; 
 const int scprice = 35;
 const int cfprice = 17;
 int b, c,d,e,f,g,h,i,j, changee;
-char ab[5];
+int ab;
 
 main()
 {
-	b = 0;
-	changee = 0;
+	//b = 0;
+	//changee = 0;
 	printf("\n\t\t\t     #BestVendingMachine2015\n");
 	printf("\t\t\t----------------------------------\n");
 	printf("\t\t\t       What are you buying?\n");
@@ -25,8 +28,8 @@ main()
 	printf("\t\t      (2) ZesMoo = 13PHP\n");
 	printf("\t\t      (3) KAPE NA HINDI PURO = 17PHP\n");
 	printf("\t\t\t----------------------------------\n");
-	
-	while (b < 1 ) //CHECKS IF THE SELECTION IS INVALID THEN RETURNS TO MAKE A SELECTION SORRY MAM KUNG DI ITO KASAMA LOL
+	printf("\t\t\t      Make a selection: "); scanf("%d", &ab); //fgets(ab, sizeof(ab), stdin); this is way better
+	/*while (b < 1 ) //CHECKS IF THE SELECTION IS INVALID THEN RETURNS TO MAKE A SELECTION SORRY MAM KUNG DI ITO KASAMA LOL
 	{
 		printf("\t\t\t      Make a selection: "); scanf("%s", &ab); //fgets(ab, sizeof(ab), stdin); this is way better
 			if (atoi(ab) > 3)
@@ -41,13 +44,15 @@ main()
 			{
 				b++;
 			}
-	}
-	switch(atoi(ab)) //atoi = alpha to integer (if u type 1 to a variable which data type is a string, it converts the data type of the variable in memory (not permanently) to integer but the value stays the same.
+	}*/
+	switch(ab) 
 	{
 		case 1:
 			printf("\t\t\t----------------------------------\n\t\t\t     You chose NUKE-A-COLA.\n\t\t\t----------------------------------\n");
 			printf("\t\t\t      TOTAL COST: %d PHP\n.", scprice);
-			while (changee < 1)
+			printf("\t\t\t    CHANGE FOR HOW MUCH?: "); scanf("%d", &c);
+
+			/*while (changee < 1)
 			{
 				printf("\t\t\t    CHANGE FOR HOW MUCH?: "); scanf("%d", &c);
 				if (c < scprice)
@@ -58,7 +63,7 @@ main()
 				{
 					changee++;
 				}
-			}
+			}*/
 			d = c - scprice;
 			e = d / 500;
 			f = ((d % 500)/100);
@@ -76,7 +81,8 @@ main()
 		case 2:
 			printf("\t\t\t----------------------------------\n\t\t\t       You chose ZesMoo.\n\t\t\t----------------------------------\n");
 			printf("\t\t\t      TOTAL COST: %d PHP\n", jpprice);
-			while (changee < 1)
+			printf("\t\t\t    CHANGE FOR HOW MUCH?: "); scanf("%d", &c);
+			/*while (changee < 1)
 			{
 				printf("\t\t\t    CHANGE FOR HOW MUCH?: "); scanf("%d", &c);
 				if (c < jpprice)
@@ -87,7 +93,7 @@ main()
 				{
 					changee++;
 				}
-			}
+			}*/
 			d = c - jpprice;
 			e = d / 500;
 			f = ((d % 500)/100);
@@ -105,7 +111,8 @@ main()
 		case 3:
 			printf("\t\t\t----------------------------------\n\t\t\t   You chose KAPE NA HINDI PURO.\n\t\t\t----------------------------------\n");
 			printf("\t\t\t       TOTAL COST: %d PHP\n", cfprice);
-			while (changee < 1)
+			printf("\t\t\t    CHANGE FOR HOW MUCH?: "); scanf("%d", &c);
+			/*while (changee < 1)
 			{
 				printf("\t\t\t    CHANGE FOR HOW MUCH?: "); scanf("%d", &c);
 				if (c < cfprice)
@@ -116,7 +123,7 @@ main()
 				{
 					changee++;
 				}
-			}
+			}*/
 			d = c - cfprice;
 			e = d / 500;
 			f = ((d % 500)/100);
@@ -132,7 +139,7 @@ main()
 			printf("\n\t\t\t%d pc(s) 500PHP \n\t\t\t%d pc(s) 100PHP \n\t\t\t%d pc(s) 50PHP \n\t\t\t%d pc(s) 10PHP \n\t\t\t%d pc(s) 5PHP \n\t\t\t%d pc(s) 1PHP",e,f,g,h,i,j); //onelineyolo
 			break;
 		default:
-			printf("i dont know how you got here but i think you might have memhack'd the variable \'a\' to get here since i have a while loop errorchecking thing up there.");
+			printf("\t\t\t----------------------------------\n\t\t\t    I n v a l i d Selection.\n\t\t\t----------------------------------\n");
 			break;
 	}
 	printf("\n\t\t\t----------------------------------\n");
