3cba5449b9b5d656e45c08c95ddf784d983c0c44
jrvmbust <jrvmbust@hotmail.com>
2015-07-15 19:50:22 +0800

    deanlist

---
+++ deanslister.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*Assignment #1 something for ITE-001*/
+/*Coded by John Rovic Vargas IT11KA3*/
+
+//deanslister
+
+int inp;
+
+main()
+{
+	printf("\n\t\t\tEnter Grade: "); scanf("%i", &inp);
+	printf("\n\t       ------------------------------------");
+	
+	switch(inp)
+	{
+		case 100:
+			printf("\n\n\t\t\t  GRADE: 1.00");
+		break;
+		case 99:
+			printf("\n\n\t\t\t  GRADE: 1.00");
+		break;
+		case 98:
+			printf("\n\n\t\t\t  GRADE: 1.00");
+		break;
+		case 97:
+			printf("\n\n\t\t\t  GRADE: 1.25");
+		break;
+		case 96:
+			printf("\n\n\t\t\t  GRADE: 1.25");
+		break;
+		case 95:
+			printf("\n\n\t\t\t  GRADE: 1.25");
+		break;
+		case 94:
+			printf("\n\n\t\t\t  GRADE: 1.50");
+		break;
+		case 93:
+			printf("\n\n\t\t\t  GRADE: 1.50");
+		break;
+		case 92:
+			printf("\n\n\t\t\t  GRADE: 1.50");
+		break;
+		case 91:
+			printf("\n\n\t\t\t  GRADE: 1.75");
+		break;
+		case 90:
+			printf("\n\n\t\t\t  GRADE: 1.75");
+		break;
+		case 89:
+			printf("\n\n\t\t\t  GRADE: 1.75");
+		break;
+		default:
+			printf("\n\n\t\tSorry, You can\'t be a dean lister ");
+		break;
+	}
+			printf("\n\n\t       ------------------------------------");
+	getch();
+}
