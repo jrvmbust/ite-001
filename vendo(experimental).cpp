1bc62890dc8f5c3438a64bcc80785355c71ad34c
jrvmbust <jrvmbust@hotmail.com>
2015-07-15 23:01:23 +0800

    Update vendo(experimental).cpp

--- vendo(experimental).cpp
+++ vendo(experimental).cpp
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <conio.h>
-#include <stdlib.h> //atoi (FOR ALPHANUMERIC INPUT CHECKING)
+#include <stdlib.h> 
 
-/*Vendo Machine Assignment #2 for ITE-001*/
-/*Coded by John Rovic Vargas IT11KA3*/
-//shoutout dyan kina mike at aaron na tumingin dito sa code ko
-// ung mga while loop kahit hindi na ilagay un. basta proper selections lang ung ilagay.
-//i wrote this code before someone asked kung pwede daw.
-//UPDATE - bawal daw while loops. s a d b o i s
+//experimental build. 
+//contains more advanced functions.
 
 const int jpprice = 13; 
 const int scprice = 35;
 const int cfprice = 17;
 int b, c,d,e,f,g,h,i,j, changee;
-int ab;
+char ab[5];
 
 main()
 {
@@ -27,10 +23,10 @@ main()
 	printf("\t\t      (2) ZesMoo = 13PHP\n");
 	printf("\t\t      (3) KAPE NA HINDI PURO = 17PHP\n");
 	printf("\t\t\t----------------------------------\n");
-	printf("\t\t\t      Make a selection: "); scanf("%d", &ab); //fgets(ab, sizeof(ab), stdin); this is way better
-	while (b < 1 ) //CHECKS IF THE SELECTION IS INVALID THEN RETURNS TO MAKE A SELECTION SORRY MAM KUNG DI ITO KASAMA LOL
+	while (b < 1 ) 
 	{
-		printf("\t\t\t      Make a selection: "); scanf("%s", &ab); //fgets(ab, sizeof(ab), stdin); this is way better
+		printf("\t\t\t      Make a selection: "); fgets(ab, sizeof(ab), stdin); //this is way better
+		
 			if (atoi(ab) > 3)
 			{
 				printf("\t\t\t----------------------------------\n\t\t\t    I n v a l i d Selection.\n\t\t\t----------------------------------\n");
@@ -44,12 +40,11 @@ main()
 				b++;
 			}
 	}
-	switch(ab) 
+	switch(atoi(ab)) 
 	{
 		case 1:
 			printf("\t\t\t----------------------------------\n\t\t\t     You chose NUKE-A-COLA.\n\t\t\t----------------------------------\n");
 			printf("\t\t\t      TOTAL COST: %d PHP\n.", scprice);
-			printf("\t\t\t    CHANGE FOR HOW MUCH?: "); scanf("%d", &c);
 
 			while (changee < 1)
 			{
@@ -77,10 +72,11 @@ main()
 			printf("\n\t\t\t----------------------------------");
 			printf("\n\t\t\t%d pc(s) 500PHP \n\t\t\t%d pc(s) 100PHP \n\t\t\t%d pc(s) 50PHP \n\t\t\t%d pc(s) 10PHP \n\t\t\t%d pc(s) 5PHP \n\t\t\t%d pc(s) 1PHP",e,f,g,h,i,j); //onelineyolo
 			break;
+			
 		case 2:
 			printf("\t\t\t----------------------------------\n\t\t\t       You chose ZesMoo.\n\t\t\t----------------------------------\n");
 			printf("\t\t\t      TOTAL COST: %d PHP\n", jpprice);
-			printf("\t\t\t    CHANGE FOR HOW MUCH?: "); scanf("%d", &c);
+			
 			while (changee < 1)
 			{
 				printf("\t\t\t    CHANGE FOR HOW MUCH?: "); scanf("%d", &c);
@@ -110,7 +106,7 @@ main()
 		case 3:
 			printf("\t\t\t----------------------------------\n\t\t\t   You chose KAPE NA HINDI PURO.\n\t\t\t----------------------------------\n");
 			printf("\t\t\t       TOTAL COST: %d PHP\n", cfprice);
-			printf("\t\t\t    CHANGE FOR HOW MUCH?: "); scanf("%d", &c);
+
 			while (changee < 1)
 			{
 				printf("\t\t\t    CHANGE FOR HOW MUCH?: "); scanf("%d", &c);
