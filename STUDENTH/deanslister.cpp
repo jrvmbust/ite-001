8906b4087997022fb8f466860b22a34886af3243
jrvmbust <jrvmbust@hotmail.com>
2015-07-17 19:49:08 +0800

    Update deanslister.cpp

--- STUDENTH/deanslister.cpp
+++ STUDENTH/deanslister.cpp
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <conio.h>
 
-/*Assignment #1 something for ITE-001*/
-/*Coded by John Rovic Vargas IT11KA3*/
-
-//deanslister
-
 int inp;
 
 main()
@@ -13,31 +8,7 @@ main()
 	printf("\n\t\t\tEnter Grade: "); scanf("%i", &inp);
 	printf("\n\t       ------------------------------------");
 	
-	if (inp >=98)
-	{
-		printf("\n\n\t\t\t  GRADE: 1.00");
-	}
-	else if (inp < 98, inp >= 95)
-	{
-		printf("\n\n\t\t\t  GRADE: 1.25");
-	}
-	else if (inp < 95, inp >=91)
-	{
-		printf("\n\n\t\t\t  GRADE: 1.50");
-	}
-	else if (inp <91, inp >=89)
-	{
-		printf("\n\n\t\t\t  GRADE: 1.75");
-	}
-	else
-	{
-		printf("\n\n\t\tSorry, You can\'t be a dean lister ");	
-	}
-		printf("\n\n\t       ------------------------------------");
-	getch();
-}
-
-/*switch(inp)
+switch(inp)
 	{
 		case 100:
 			printf("\n\n\t\t\t  GRADE: 1.00");
@@ -78,4 +49,8 @@ main()
 		default:
 			printf("\n\n\t\tSorry, You can\'t be a dean lister ");
 		break;
-	}*/
+	}
+	getch();
+}
+
+/**/
