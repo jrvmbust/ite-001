c8cb0f5ed4f92eec8ec1cb77cd1cc9b9cd72524c
jrvmbust <jrvmbust@hotmail.com>
2015-08-09 00:51:56 +0800

    as4

---
+++ mt_as4.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <conio.h>
+
+int a,b,c,e;
+
+main()
+{
+	printf("input height: "); scanf("%d", &a);
+	printf("input width: "); scanf("%d", &b);
+	printf("\n");
+	for(c =0;c<a;c++)
+	{
+			if (c==a-1||c==0) //prints the first line and the last line with all dots
+			{
+				for(int d =0;d<b;d++)
+				{
+					printf("*");	
+				}
+			printf("\n");
+			}
+			
+			else if (b==1) //prints the dots if width = 1
+			{
+				for(int d =0;d<b;d++)
+				{
+					printf("*");	
+				}
+			printf("\n");
+			}
+			
+			else //ghetto spacing 
+			{
+				printf("*");
+				for(e=0;e<b-2;e++)
+				{
+					printf(" ");
+				}
+				printf("*");
+				printf("\n");
+			}	
+	}
+	getch();
+}
+		
+		
+		
+
+	
+
