43fe1403c7c7ca4e534cc9de5602b265458ef02b
jrvmbust <jrvmbust@hotmail.com>
2015-08-08 19:36:43 +0800

    more non functiorino based lol

--- mt_as3.cpp
+++ mt_as3.cpp
@@ -10,11 +10,11 @@ main()
 	
 	for (a=0;a <43;a++)
 	{
-		if (isdigit(sentence[a]))
+		if (sentence[a] == '0' ||sentence[a] == '1' ||sentence[a] == '2' ||sentence[a] == '3' ||sentence[a] == '4' ||sentence[a] == '5' ||sentence[a] == '6' ||sentence[a] == '7' ||sentence[a] == '8' ||sentence[a] == '9')
 		{
 			b++;
 		}
-		else if (isalpha(sentence[a]))
+		else if (sentence[a] == 'a'||sentence[a] == 'b'||sentence[a] == 'c'||sentence[a] == 'd'||sentence[a] == 'e'||sentence[a] == 'f'||sentence[a] == 'g'||sentence[a] == 'h'||sentence[a] == 'i'||sentence[a] == 'j'||sentence[a] == 'k'||sentence[a] == 'l'||sentence[a] == 'm'||sentence[a] == 'n'||sentence[a] == 'o'||sentence[a] == 'p'||sentence[a] == 'q'||sentence[a] == 'r'||sentence[a] == 's'||sentence[a] == 't'||sentence[a] == 'u'||sentence[a] == 'v'||sentence[a] == 'w'||sentence[a] == 'x'||sentence[a] == 'y'||sentence[a] == 'z')
 		{
 			c++;
 		}
